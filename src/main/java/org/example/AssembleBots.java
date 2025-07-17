0a7fbdb7e7ad9a51d33896ecdfbdce5a79f36ebd
Raza Imam <raza.imam@cerner.com>
2025-07-17 14:42:55 +0100

    Optimizing AssembleBots

--- src/main/java/org/example/AssembleBots.java
+++ src/main/java/org/example/AssembleBots.java
@@ -1,126 +1,74 @@
 package org.example;
 
-import java.io.*;
 import java.util.*;
-
 
 public class AssembleBots {
 
+    /**
+     * Problem:
+     * We have a list of parts, where each part belongs to a robot and is in the format: "RobotName_part".
+     * Given a list of all parts and the required parts to assemble a full robot,
+     * return a list of robot names that can be fully assembled.
+     *
+     * Example:
+     * allParts = ["Rosie_claw", "Rosie_sensors", "Optimus_case", ...]
+     * requiredParts = "sensors,case,speaker,wheels"
+     * Output: ["Optimus", "Rosie"]
+     */
 
-    /*
-We have a bin of robot parts in a factory. Each part goes to a robot with a specific, unique name. Each part will be described by a string, with the name of the robot and the part name separated by an underscore, like "Rosie_arm".
+    public static void main(String[] args) {
+        String[] allParts = {
+                "Rosie_claw", "Rosie_sensors", "Dustie_case", "Optimus_sensors",
+                "Rust_sensors", "Rosie_case", "Rust_case", "Optimus_speaker",
+                "Rosie_wheels", "Rosie_speaker", "Dustie_case", "Dustie_arms",
+                "Rust_claw", "Dustie_case", "Dustie_speaker", "Optimus_case",
+                "Optimus_wheels", "Rust_legs", "Optimus_sensors"
+        };
 
-All robots are made of the same types of parts, and we have a string of all of the parts required to form a complete robot. Given a list of available parts, return the collection of robot names for which we can build at least one complete robot.
+        String required1 = "sensors,case,speaker,wheels";
+        String required2 = "sensors,case,speaker,wheels,claw";
+        String required3 = "sensors,case,screws";
 
-Sample Input:
-
-all_parts = [
-    "Rosie_claw",
-    "Rosie_sensors",
-    "Dustie_case",
-    "Optimus_sensors",
-    "Rust_sensors",
-    "Rosie_case",
-    "Rust_case",
-    "Optimus_speaker",
-    "Rosie_wheels",
-    "Rosie_speaker",
-    "Dustie_case",
-    "Dustie_arms",
-    "Rust_claw",
-    "Dustie_case",
-    "Dustie_speaker",
-    "Optimus_case",
-    "Optimus_wheels",
-    "Rust_legs",
-    "Optimus_sensors" ]
-
-required_parts_1 = "sensors,case,speaker,wheels"
-
-- iteration:
-   sensors: list of robots [A, B,  C]
-     case:  list of robots [B, E, F]
-      ? (two lists) = ??
-
-required_parts_2 = "sensors,case,speaker,wheels,claw"
-required_parts_3 = "sensors,case,screws"
-
-Expected Output (output can be in any order):
-
-get_robots(all_parts, required_parts_1) => ["Optimus", "Rosie"]
-get_robots(all_parts, required_parts_2) => ["Rosie"]
-get_robots(all_parts, required_parts_3) => []
-
-N: Number of elements in all_parts
-P: Number of elements in required_parts
-*/
-
-
-
-
-        public static void main(String[] argv) {
-            String required_parts_1 = "sensors,case,speaker,wheels";
-            String required_parts_2 = "sensors,case,speaker,wheels,claw";
-            String required_parts_3 = "sensors,case,screws";
-
-            String[] all_parts = {
-                    "Rosie_claw",
-                    "Rosie_sensors",
-                    "Dustie_case",
-                    "Optimus_sensors",
-                    "Rust_sensors",
-                    "Rosie_case",
-                    "Rust_case",
-                    "Optimus_speaker",
-                    "Rosie_wheels",
-                    "Rosie_speaker",
-                    "Dustie_case",
-                    "Dustie_arms",
-                    "Rust_claw",
-                    "Dustie_case",
-                    "Dustie_speaker",
-                    "Optimus_case",
-                    "Optimus_wheels",
-                    "Rust_legs",
-                    "Optimus_sensors"};
-
-            List<String> pbots =   getBot(all_parts, required_parts_1);
-            System.out.println(pbots);
-            Arrays.asList();
-
-        }
-        public static List<String> getBot(String[] all_parts, String required_parts_1){
-            Map<String, List<String>> partTobotsMap = new HashMap();
-
-            Set<String> botset = new HashSet();
-
-            for(String str : all_parts){
-                String[] strs =  str.split("_");
-                botset.add(strs[0]);
-                List<String> botList = partTobotsMap.containsKey(strs[1]) ? partTobotsMap.get(strs[1]) : new ArrayList<>();
-                botList.add(strs[0]);
-                partTobotsMap.put(strs[1], botList);
-            }
-            List<String> pbots = new ArrayList<>();
-
-            for(String bot : botset ) {
-                boolean notThebot = false;
-                for(String part : required_parts_1.split(",")) {
-                    List<String> botHasParts = partTobotsMap.get(part);
-                    if( botHasParts.contains(bot)){
-                        continue;
-                    } else {
-                        notThebot = true;
-                        break;
-                    }
-                }
-                if(!notThebot){
-                    pbots.add(bot);
-                }
-            }
-
-            return pbots;
-
-        }
+        System.out.println(getBots(allParts, required1)); // [Rosie, Optimus]
+        System.out.println(getBots(allParts, required2)); // [Rosie]
+        System.out.println(getBots(allParts, required3)); // []
     }
 
+    /**
+     * Returns a list of robot names that can be fully assembled.
+     *
+     * @param allParts       Array of parts in "RobotName_part" format.
+     * @param requiredParts  Comma-separated string of required parts.
+     * @return List of robot names that have all required parts.
+     */
+    public static List<String> getBots(String[] allParts, String requiredParts) {
+        // Map<RobotName, Set<PartName>> to store each robot and the parts it has
+        Map<String, Set<String>> robotPartsMap = new HashMap<>();
+
+        // Build the robot-to-parts map
+        for (String item : allParts) {
+            String[] split = item.split("_"); // e.g., "Rosie_claw" → ["Rosie", "claw"]
+            String robotName = split[0];
+            String partName = split[1];
+
+            // Add the part to this robot's set
+            robotPartsMap.putIfAbsent(robotName, new HashSet<>());
+            robotPartsMap.get(robotName).add(partName);
+        }
+
+        // Convert required parts into a Set for quick lookup
+        Set<String> requiredSet = new HashSet<>(Arrays.asList(requiredParts.split(",")));
+
+        // Result list of robots that meet the requirement
+        List<String> eligibleRobots = new ArrayList<>();
+
+        // Check each robot if it has all required parts
+        for (Map.Entry<String, Set<String>> entry : robotPartsMap.entrySet()) {
+            if (entry.getValue().containsAll(requiredSet)) {
+                eligibleRobots.add(entry.getKey());
+            }
+        }
+
+        return eligibleRobots;
+    }
+}
